b45a9fb5848bed62f04bfea20b65c5f6d95afa98
alexpshkov <alexpshkov@gmail.com>
2025-02-04 17:16:58 +0300

    change ports for containers in lab1

--- lab1/start.bat
+++ lab1/start.bat
@@ -2,5 +2,5 @@
 
 docker build -t app-2048 .
 
-docker run -d -p 8080:8080 --name app-2048-container-1 app-2048
-docker run -d -p 8085:8080 --name app-2048-container-2 app-2048
+docker run -d -p 1440:8080 --name app-2048-container-1 app-2048
+docker run -d -p 1441:8080 --name app-2048-container-2 app-2048
